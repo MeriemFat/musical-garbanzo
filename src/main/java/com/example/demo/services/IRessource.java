3043593451f8594704ef278b2d602efb724e2b03
DJEBBI Maram <maram.djebbi@soprahr.com>
2023-07-20 09:28:36 +0100

    ressources

--- src/main/java/com/example/demo/services/IRessource.java
+++ src/main/java/com/example/demo/services/IRessource.java
@@ -1,7 +1,16 @@
 package com.example.demo.services;
 
+import com.example.demo.entites.Ressources;
+import com.example.demo.entites.projet;
 import org.springframework.stereotype.Component;
-
+import java.util.List;
 @Component
 public interface IRessource {
+
+    List<Ressources> getAllRessources();
+
+    void removeRessouces(Long idRessources);
+
+
+
 }
--- src/main/java/com/example/demo/services/RessourceServices.java
+++ src/main/java/com/example/demo/services/RessourceServices.java
@@ -1,13 +1,29 @@
 package com.example.demo.services;
 
+import com.example.demo.entites.Ressources;
 import com.example.demo.repository.RessourceRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class RessourceServices implements IRessource{
     @Autowired
     RessourceRepo ressourceRepository;
+
+
+    @Override
+    public List<Ressources> getAllRessources() {
+        return ressourceRepository.findAll();
+    }
+
+    @Override
+    public void removeRessouces(Long idRessources) {
+        ressourceRepository.deleteById(idRessources);
+    }
+
+
 }
